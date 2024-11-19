8f7f5fb7d93fc20c548723d963667d116b47eb63
Estelle Morelli <estelle.morelli@oclock.school>
2024-11-19 13:11:29 +0100

    avec authentification

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\DateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 
 Route::get('/test', function () {
@@ -20,3 +21,11 @@ Route::put('/unavailables/{id}', [DateController::class, 'update'])->where('id',
 Route::patch('/unavailables/{id}', [DateController::class, 'update'])->where('id', '[0-9]+');
 
 Route::delete('/unavailables/{id}', [DateController::class, 'delete'])->where('id', '[0-9]+');
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth.jwt');
+
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth.jwt');
